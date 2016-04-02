983e07bd7127ce0f2bf3b31faf782331fe3e507b
khushhallchandra <kcm00000@gmail.com>
2016-04-02 23:41:40 +0530

    Started

---
+++ README.md
@@ -0,0 +1,2 @@
+# EE338 Digital Signal Processing
+This repo contains code for the Filter Design Assignment
---
+++ bandPassFilter/IIRfliter.m
@@ -0,0 +1,23 @@
+%% IIR filter
+
+%% The specifications are 
+
+% - M = 141 (assigned to me)
+% - Monotonic in stopband 
+% - Equiripple in passband
+% - Delta_p = Delta_s = 0.15
+% - Transtion band = 2kHertz
+
+% - w_s1 = 2pi * 0.182
+% - w_p1 = 2pi * 0.202
+% - w_p1 = 2pi * 0.302
+% - w_s2 = 2pi * 0.322
+
+% - W_s1 = 0.6435
+% - W_p1 = 0.7362
+% - W_p1 = 1.3947
+% - W_s2 = 1.5978
+% - Convert to low pass filter specs
+% - W_l_p = 1
+% - W_l_s = 1.4505
+
