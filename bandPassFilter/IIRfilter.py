064f4e0cd005af66bd6ec09ad97d7506da4ac895
khushhallchandra <kcm00000@gmail.com>
2016-04-05 23:55:54 +0530

    Added iir filter for bandpass

---
+++ bandPassFilter/IIRfilter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pylab
+
+#Given specification
+
+M = 102
+delta_p = 0.15
+delta_s = 0.15
+f_sample = 100000
+
+# analog values for bandpass
+
+omega_s1 = 18.2*1000
+omega_p1 = 20.2*1000
+omega_p2 = 30.2*1000
+omega_s2 = 32.2*1000
